50b05484422e23ce5459d933923cbc5ee755bd9c
0x7EVEN <its.anonymus149@gmail.com>
2021-09-12 17:32:11 +0530

    cleaned App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 function App () {
   return (
     <div className="App">
-      <h1>this is initial commit for website - portfolio of Arif.</h1>
     </div>
   );
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar () {
+     return (
+          <div>
+               <h1>This is test portfolio of Arif</h1>
+          </div>
+     )
+}
