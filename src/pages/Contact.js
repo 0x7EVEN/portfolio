3c353a2a8d3d06a2821c2c4934ef17f8931c4b22
0x7even <its.anonymus149@gmail.com>
2022-05-14 23:15:32 +0530

    whole new template thanks to michel

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ import ContactIcons from '../components/Contact/ContactIcons';
 const Contact = () => (
   <Main
     title="Contact"
-    description="Contact Michael D'Angelo via email @ michael.l.dangelo@gmail.com"
+    description="Contact Arif Shaikh via email @ ar1fshaikh@protonmail.com"
   >
     <article className="post" id="contact">
       <header>
