6e5ef2123ac13c5c55de2e678a00b66da54c80af
0x7even <its.anonymus149@gmail.com>
2022-05-23 13:06:46 +0530

    added cell

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
 
-import Cell from '../components/projects/Cell.js';
+import Cell from '../components/projects/Cell';
 import data from '../data/projects';
 
 const Projects = () => (
