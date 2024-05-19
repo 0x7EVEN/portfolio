a0018b86718982700e43189918e78fb793ce66bb
ar1fshaikh <its.anonymus149@gmail.com>
2024-05-19 08:55:40 +0000

    updated job description for security

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -25,9 +25,12 @@ const positions = [
     link: 'https://olacabs.com',
     daterange: 'December 2021 - Apr 2023',
     points: [
-      'Worked with purchase flow of olaelectric',
-      'Built production, on-line, end-to-end optimized websites with React, Vue etc.',
-      'Worked with deployments and automation',
+      'Vulnerability assessments and penetration tests on a wide range of web applications, leveraging advanced tools and techniques to identify critical security flaws.',
+      'Conducted in-depth analysis on emerging vulnerabilities and attack vectors, staying in sync with the latest trends and developments in the cybersecurity landscape.',
+      'Contributed in security audits for regulatory compliance, including RBI guidelines, by contributing to code review documentation and enhancing overall security posture.',
+      'Led the Bug Bounty program, fostering collaboration with external security researchers and significantly improving organization\'s response and reach to high-severity vulnerabilities.',
+      'Experienced in partnering with developers to understand vulnerabilities by secure coding webinars, implement effective solutions, and strengthen security, leading to a 30% decrease in security incidents.',
+      'Hands-on experience with Vultr, AWS, and Linode platforms for conducting cloud configuration reviews.',
     ],
   },
 ];
